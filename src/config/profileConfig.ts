1498bd867ec9b0ac10a3cf803c4e84aab9eb21c7
dahailaila <1404455722@qq.com>
2026-06-13 23:55:47 +0800

    更新内容

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -6,13 +6,13 @@ export const profileConfig: ProfileConfig = {
 	// 1. public 目录（以 "/" 开头，不优化）："/assets/images/avatar.webp"
 	// 2. src 目录（不以 "/" 开头，自动优化但会增加构建时间，推荐）："assets/images/avatar.webp"
 	// 3. 远程 URL："https://example.com/avatar.jpg"
-	avatar: "assets/images/avatar.avif",
+	avatar: "assets/images/avatar.jpg",
 
 	// 名字
-	name: "Firefly",
+	name: "花海",
 
 	// 个人签名
-	bio: "Hello, I'm Firefly.",
+	bio: "花海的小窝.",
 
 	// 链接配置
 	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
@@ -30,7 +30,7 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "GitHub",
 			icon: "fa7-brands:github",
-			url: "https://github.com/CuteLeaf",
+			url: "https://github.com/dahailaila",
 			showName: false,
 		},
 		{
@@ -39,11 +39,6 @@ export const profileConfig: ProfileConfig = {
 			url: "mailto:xiaye@msn.com",
 			showName: false,
 		},
-		{
-			name: "RSS",
-			icon: "fa7-solid:rss",
-			url: "/rss/",
-			showName: false,
-		},
+		
 	],
 };
