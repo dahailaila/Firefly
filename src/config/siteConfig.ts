52befb099f6ce6b156a55a78382388905863d918
dahailaila <1404455722@qq.com>
2026-06-13 22:45:20 +0800

    更新内容

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -6,27 +6,37 @@ const SITE_LANG = "zh_CN";
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "Firefly",
+	title: "花海博客",
 
 	// 站点副标题
-	subtitle: "Demo site",
+	subtitle: "记录生活，分享美好！",
 
 	// 站点 URL
-	site_url: "https://firefly.cuteleaf.cn",
-
+	site_url: "https://hhwzk.cc.cd",
 	// 站点描述
 	description:
-		"Firefly 是一款基于 Astro 框架和 Fuwari 模板开发的清新美观且现代化个人博客主题模板，专为技术爱好者和内容创作者设计。该主题融合了现代 Web 技术栈，提供了丰富的功能模块和高度可定制的界面，让您能够轻松打造出专业且美观的个人博客网站。",
+		"本站是一个专注分享优质资源的平台，致力于分享优质源码,副业教程，建站技术，软件工具，自学课程，操作系统，经验教程，实用素材等各个领域的资源型站点，为您提供建站或其他方面的便利。是一个十分值得收藏的网站",
 
 	// 站点关键词
 	keywords: [
+		
+		"花海博客",
+		"技术博客",
+		"静态博客",
+		"建站源码",
+		"源码搭建",
+		"建站教程",
+		"源码教程",
+		"视频教程",
+		"建站技术",
+		"软件工具",
+		"副业教程",
+		"实用素材",
 		"Firefly",
 		"Fuwari",
 		"Astro",
 		"ACGN",
-		"博客",
-		"技术博客",
-		"静态博客",
+
 	],
 
 	// 主题色
@@ -78,7 +88,7 @@ export const siteConfig: SiteConfig = {
 			alt: "🍀",
 		},
 		// 导航栏标题
-		title: "Firefly",
+		title: "花海博客",
 		// 全宽导航栏，导航栏是否占满屏幕宽度
 		widthFull: false,
 		// 导航菜单对齐方式，left：左对齐，center：居中
@@ -90,7 +100,7 @@ export const siteConfig: SiteConfig = {
 	},
 
 	// 站点开始日期，用于统计运行天数
-	siteStartDate: "2025-01-01",
+	siteStartDate: "2026-06-13",
 
 	// 站点时区（IANA 时区字符串），用于格式化bangumi、rss里的构建日期时间等等..
 	// 示例："Asia/Shanghai", "UTC", 如果为空，则按照构建服务器的时区进行时区转换
