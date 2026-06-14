187a052bf060496817254634ac2feacdef5c5396
dahailaila <1404455722@qq.com>
2026-06-14 22:23:27 +0800

    添加友链自助

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -24,7 +24,7 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "qq",
 			icon: "fa7-brands:qq",
-			url: "https://qm.qq.com/q/ZGsFa8qX2G",
+			url: "https://hhwzk.cc.cd",
 			showName: false,
 		},
 		{
@@ -36,7 +36,7 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "Email",
 			icon: "fa7-solid:envelope",
-			url: "mailto:xiaye@msn.com",
+			url: "https://hhwzk.cc.cd",
 			showName: false,
 		},
 		
