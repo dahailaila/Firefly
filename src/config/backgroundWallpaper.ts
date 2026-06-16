6dfd2715da9eff82384c7a682d639ec20ee368c0
dahailaila <1404455722@qq.com>
2026-06-16 11:00:21 +0800

    更新内容

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -41,22 +41,15 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 		// 桌面背景图片（支持单张或多张随机）
 		// desktop: "assets/images/DesktopWallpaper/d1.avif",
 		desktop: [
-			"assets/images/DesktopWallpaper/d1.avif",
-			"assets/images/DesktopWallpaper/d2.avif",
-			"assets/images/DesktopWallpaper/d3.avif",
-			"assets/images/DesktopWallpaper/d4.avif",
-			"assets/images/DesktopWallpaper/d5.avif",
-			"assets/images/DesktopWallpaper/d6.avif",
+			"assets/images/DesktopWallpaper/云朵-城镇-好看.png",
+			"assets/images/DesktopWallpaper/云彩-夜晚-夜景.png",			
+			
 		],
 		// 移动背景图片（支持单张或多张随机）
 		// mobile: "assets/images/MobileWallpaper/m1.avif",
 		mobile: [
-			"assets/images/MobileWallpaper/m1.avif",
-			"assets/images/MobileWallpaper/m2.avif",
-			"assets/images/MobileWallpaper/m3.avif",
-			"assets/images/MobileWallpaper/m4.avif",
-			"assets/images/MobileWallpaper/m5.avif",
-			"assets/images/MobileWallpaper/m6.avif",
+			"assets/images/MobileWallpaper/小白-木桥-田野.png",
+			
 		],
 	},
 	// 横幅壁纸和全屏壁纸共享配置
@@ -70,17 +63,17 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 			// 是否允许用户通过控制面板切换横幅标题显示
 			switchable: true,
 			// 主页横幅主标题
-			title: "Lovely home",
+			title: "花海のblog",
 			// 主页横幅主标题字体大小
 			titleSize: "3.8rem",
 			// 主页横幅副标题
 			subtitle: [
-				"In Reddened Chrysalis, I Once Rest",
-				"From Shattered Sky, I Free Fall",
-				"Amidst Silenced Stars, I Deep Sleep",
-				"Upon Lighted Fyrefly, I Soon Gaze",
-				"From Undreamt Night, I Thence Shine",
-				"In Finalized Morrow, I Full Bloom",
+				"顺为凡，逆则仙，只在心中一念间。",
+				"不惧本心，方为自我，不违本心，是为自在。",
+				"雨生于天，死于大地，中间下落的过程，便是人生。",
+				"雨落一生，短暂却灿烂，人生亦是如此，哪怕前路荆棘，也要走完全程。",
+				"这世上，没有无缘无故的恨，也没有无缘无故的爱。",
+				"我花海此生，不拜天地，不敬鬼神，只拜父母。",
 			],
 			// 主页横幅副标题字体大小
 			subtitleSize: "1.5rem",
