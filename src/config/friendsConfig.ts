4f1f4ee03381d7f7087e88e4fbe69df1475ab545
dahailaila <1404455722@qq.com>
2026-06-22 13:36:52 +0800

    更新内容-6.22

--- src/config/friendsConfig.ts
+++ src/config/friendsConfig.ts
@@ -27,7 +27,7 @@ export const friendsConfig: FriendLink[] = [
 		imgurl:
 			"https://weavatar.com/avatar/d252655d40d6874417a720bad0a6c5f77f8f6a1fd2f882f8f338402dc37e4190?s=640",
 		desc: "个人技术博客",
-		siteurl: "https://hhwzkcc.cd",
+		siteurl: "https://hhwzk.cc.cd",
 		tags: ["Blog"],
 		weight: 10, // 权重，数字越大排序越靠前
 		enabled: true, // 是否启用
